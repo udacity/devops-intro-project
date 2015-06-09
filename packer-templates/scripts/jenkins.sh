3e81bd1c536ab7b860db0fe28a04a8c26261c840
Gundega Dekena <gundega@udacity.com>
2015-06-09 17:52:17 +0300

    add(jenkins): automatic plugin installation

--- packer-templates/scripts/jenkins.sh
+++ packer-templates/scripts/jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 # JDK and JRE are required for Jenkins
-apt-get install -y openjdk-7-jre openjdk-7-jdk 
+apt-get install -y openjdk-7-jre openjdk-7-jdk unzip
 
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
 echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list
@@ -12,17 +12,18 @@ apt-get upgrade
 
 # copy premade configuration files
 # jenkins default config, to set --prefix=jenkins
-cp -f /tmp/jenkins-config/jenkins /etc/defaults
+cp -f /tmp/jenkins-config/jenkins /etc/default
+# install some extra plugins
+/bin/bash /tmp/jenkins-config/install_jenkins_plugins.sh
 # jenkins security and pipeline plugin config
 cp -f /tmp/jenkins-config/config.xml /var/lib/jenkins
 # set up username for vagrant
-mkdir /var/lib/jenkins/users/vagrant
+mkdir -p /var/lib/jenkins/users/vagrant
 cp /tmp/jenkins-config/users/vagrant/config.xml /var/lib/jenkins/users/vagrant
 # example job
-cd /tmp/jenkins-config
+mkdir -p /var/lib/jenkins/jobs
+cd /var/lib/jenkins/jobs
 tar zxvf /tmp/jenkins-config/example-job.tar.gz
-cp -Rf /tmp/jenkins-config/git-example /var/lib/jenkins/jobs
-
 
 # set permissions or else jenkins can't run jobs
 chown -R jenkins:jenkins /var/lib/jenkins
